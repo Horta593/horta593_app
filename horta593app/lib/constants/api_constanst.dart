84016b02e014258e52d22dec49ec9b4272fab320
adelovelace <anmero@fiec.espol.edu.ec>
2023-08-13 23:51:36 -0500

    feat: update the apiurl

--- horta593app/lib/constants/api_constanst.dart
+++ horta593app/lib/constants/api_constanst.dart
@@ -1,5 +1,5 @@
 class API {
-  static const String BASE_URL = "http://10.0.2.2:3000";
+  static const String BASE_URL = "http://44.212.161.237:3000/";
 
   static const String LOGIN_ENDPOINT = '/auth/signin';
 }
