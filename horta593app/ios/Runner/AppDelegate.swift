5a1cab15b72602e26cba462693d477072ef23351
adelovelace <anmero@fiec.espol.edu.ec>
2023-08-14 02:16:19 -0500

    fix: merge with the sign up and conection to the server successfully!

--- horta593app/ios/Runner/AppDelegate.swift
+++ horta593app/ios/Runner/AppDelegate.swift
@@ -4,11 +4,10 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-      _ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-      GMSServices.provideAPIKey("AIzaSyA-hFOwPzgxn-E80MWw2VSrAgsGS_Lt9Ak")
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
