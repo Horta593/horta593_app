33a386c05e674805e7bc8156dba60434b4909410
Milca <milca@MacBook-Pro-de-user.local>
2023-08-14 09:52:30 -0500

    fix(ios): try to fix dependencies

---
+++ horta593app/hortaapp/hortaapp/hortaappApp.swift
@@ -0,0 +1,17 @@
+//
+//  hortaappApp.swift
+//  hortaapp
+//
+//  Created by Milca on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct hortaappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
